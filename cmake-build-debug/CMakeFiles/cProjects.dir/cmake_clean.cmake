c3ddcbe27ddc0822816fa7a8f97a1256d56339d8
baileywickham <baileywickham@gmail.com>
2018-01-30 08:20:34 -0800

    added cmake files

---
+++ cmake-build-debug/CMakeFiles/cProjects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cProjects.dir/main.cpp.o"
+  "cProjects.pdb"
+  "cProjects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
